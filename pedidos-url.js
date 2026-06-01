e4536ffc7ee72cc08adcb07b5292d04309244ef9
nacht-t <contacto@gmexpress.cl>
2026-06-01 15:57:49 -0400

    actualización del catálogo web

--- pedidos-url.js
+++ pedidos-url.js
@@ -1,5 +1,5 @@
 window.CATALOGO_ORDERS_WEBAPP_URL =
-  "https://script.google.com/macros/s/AKfycbz8IWb6yxA0Z01Z4GM0onPd_cpJSAOTt0zyu8Id5afTbMzffduMp3UmMU8BbPbpcrM/exec";
+  "https://script.google.com/macros/s/AKfycbz8IWb6yxA0Z01Z4GM0onPd_cpJSAOTt0zyu8Id5afTbMzffduMp3UmMU8BbPbpcrM/exec"
 
 window.CATALOGO_CALLMEBOT_WHATSAPP = {
   phone: "56947854598",
